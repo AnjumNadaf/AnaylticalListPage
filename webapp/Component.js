0a6c89dab8d53ff06cfc262eb4beaf0d1316ffc3
AnjumNadaf <anjunadaf7@gmail.com>
2021-01-29 17:59:52 +0530

    'chages'

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(['sap/suite/ui/generic/template/lib/AppComponent'], function(AppComponent) {
+	return AppComponent.extend("alp.AnlyticalListPage.Component", {
+		metadata: {
+			manifest: "json"
+		}
+	});
+});
